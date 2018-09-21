12c86947aab209cd81a1b0515c0a500f29b65fa2
AidyBao <aidybao555@sina.com>
2018-09-21 14:25:39 +0800

    Initial Commit

---
+++ MQCompressImageDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MQCompressImageDemo
+//
+//  Created by 120v on 2018/9/21.
+//  Copyright © 2018年 MQ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
